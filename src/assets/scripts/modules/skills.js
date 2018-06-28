6281f216b88f8cb0b9d1ca3e202383159d3dc322
Tatyana Kovylina <tkovylina@gmail.com>
2018-06-28 22:25:41 +0300

    skills and slider animation

--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -14,10 +14,25 @@ const skill = {
       );
       const percents = (dashOffset / 100) * (100 - this.skillPercent);
       circle.style.strokeDashoffset = percents;
+    },
+    handleScroll(event) {
+      const height = document.documentElement.clientHeight / 2;
+      const skillList = document.querySelector(".skills-list");
+
+      if (height > skillList.getBoundingClientRect().top) {
+        this.drawCircle();
+        window.removeEventListener("scroll", this.handleScroll);
+      }
     }
   },
-  mounted() {
-    this.drawCircle();
+  // mounted() {
+  //   this.drawCircle();
+  // }
+  created: function() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed: function() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 
